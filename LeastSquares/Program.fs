5d09305202ccd1cf83b58500a4f89efa39573905
Derek Lane <dglane001@gmail.com>
2018-12-18 14:34:56 -0600

    preparing to consolidate dLoss_dParam

--- LeastSquares/Program.fs
+++ LeastSquares/Program.fs
@@ -5,15 +5,7 @@ open Microsoft.FSharp.Quotations
 // See the 'F# Tutorial' project for more help.
 
 [<EntryPoint>]
-let main argv = 
-    Seq.unfold 
-        SlopeInterceptObjective.update 
-            ([SlopeInterceptObjective.initSlope; SlopeInterceptObjective.initOffset], 
-                SlopeInterceptObjective.loss [SlopeInterceptObjective.initSlope; SlopeInterceptObjective.initOffset])
-    |> List.ofSeq
-    |> List.last
-    |> function
-        ([finalSlope; finalOffset], finalLoss) ->
-            printfn "final values = %A" (SlopeInterceptObjective.currentValueFromSlopeOffset SlopeInterceptObjective.iter0 [finalSlope; finalOffset])    
+let main argv =     
+    SlopeInterceptObjective.optimize |> ignore
 
     0 // return an integer exit code
--- LeastSquares/SlopeInterceptObjective.fs
+++ LeastSquares/SlopeInterceptObjective.fs
@@ -8,35 +8,34 @@ let currentValueFromSlopeOffset init [slope; offset] =
     |> Array.map ((*) slope)
     |> Array.map ((+) offset)
 
+           
+// calculate loss function as squared sum
+let loss target init [slope; offset] =
+    let update = currentValueFromSlopeOffset init [slope; offset]
+    (target, update) 
+        ||> Array.map2 (-) 
+        |> Array.sumBy (fun x -> x*x)
+
 let target = [| 0.0; 3.0; 5.0; -2.0; |]
 
 let iter0 = Helper.genRandomNumbers 4
-    
+
 printfn "target:        %A" target
 printfn "init:          %A" iter0
     
 let initSlope = 1.0
 let initOffset = 0.0
-       
-// calculate loss function as squared sum
-let loss [slope; offset] =
-    let update = currentValueFromSlopeOffset iter0 [slope; offset]
-    (target, update) 
-        ||> Array.map2 (-) 
-        |> Array.sumBy (fun x -> x*x)
     
-printfn "initial loss %f" (loss [initSlope; initOffset])
+printfn "initial loss %f" (loss target iter0 [initSlope; initOffset])
 
 let dSlope_dLoss [slope; offset] = 
-    let d = (loss [slope+LeastSqOptimizer.delta; offset]) - (loss [slope; offset])
-    let d = LeastSqOptimizer.delta / if d < 0.0 then d - 0.1 else d + 0.1
-    d
+    let d = (loss target iter0 [slope + LeastSqOptimizer.delta; offset]) - (loss target iter0 [slope; offset])
+    LeastSqOptimizer.delta / if d < 0.0 then d - 0.1 else d + 0.1
     // iter0 |> Array.map ((*) 1.0)
 
 let dOffset_dLoss [slope; offset] =
-    let d = (loss [slope; offset+LeastSqOptimizer.delta]) - (loss [slope; offset])
-    let d = LeastSqOptimizer.delta / if d < 0.0 then d - 0.1 else d + 0.1
-    d
+    let d = (loss target iter0 [slope; offset + LeastSqOptimizer.delta]) - (loss target iter0 [slope; offset])
+    LeastSqOptimizer.delta / if d < 0.0 then d - 0.1 else d + 0.1
     // iter0 |> Array.map (fun x -> 1.0)
     
 let update ([currentSlope; currentOffset], currentLoss) = 
@@ -44,7 +43,7 @@ let update ([currentSlope; currentOffset], currentLoss) =
     let dOffset = dOffset_dLoss [currentSlope; currentOffset]
     let updateSlope = currentSlope - LeastSqOptimizer.rate * dSlope
     let updateOffset = currentOffset - LeastSqOptimizer.rate * dOffset
-    let updateLoss = loss [updateSlope; updateOffset]
+    let updateLoss = loss target iter0 [updateSlope; updateOffset]
 
     printfn "updated slope = %f (%4.2f %%), offset = %f (%4.2f %%), loss = %f" 
         updateSlope (100.0 * abs(dSlope)/(LeastSqOptimizer.delta + abs(currentSlope)))
@@ -55,9 +54,10 @@ let update ([currentSlope; currentOffset], currentLoss) =
     then None
     else Some (([updateSlope; updateOffset], updateLoss), ([updateSlope; updateOffset], updateLoss))
 
-Seq.unfold update ([initSlope; initOffset], loss [initSlope; initOffset])
-|> List.ofSeq
-|> List.last
-|> function
-    ([finalSlope; finalOffset], finalLoss) ->
-        printfn "final values = %A" (currentValueFromSlopeOffset iter0 [finalSlope; finalOffset])
+let optimize = 
+    Seq.unfold update ([initSlope; initOffset], loss target iter0 [initSlope; initOffset])
+    |> List.ofSeq
+    |> List.last
+    |> function
+        ([finalSlope; finalOffset], finalLoss) ->
+            printfn "final values = %A" (currentValueFromSlopeOffset iter0 [finalSlope; finalOffset])
