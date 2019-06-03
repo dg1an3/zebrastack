a761a0b6b1578ca89efcfc66b76a40f3527e867e
Derek Lane <dglane001@gmail.com>
2019-06-03 18:16:23 -0500

    set switch between mnist and cifar10

--- keras_autoencoder/keras_autoencoder.py
+++ keras_autoencoder/keras_autoencoder.py
@@ -5,8 +5,12 @@ from keras.datasets import mnist
 from keras.datasets import cifar10
 
 import numpy as np
-# (x_train, _), (x_test, _) = mnist.load_data()
-(x_train, _), (x_test, _) = cifar10.load_data()
+use_cifar10 = False
+if use_cifar10:
+    (x_train, _), (x_test, _) = cifar10.load_data()
+else:
+    (x_train, _), (x_test, _) = mnist.load_data()
+
 x_train = x_train.astype('float32') / 255.
 x_test = x_test.astype('float32') / 255.
 x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
@@ -14,8 +18,10 @@ x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
 print(x_train.shape)
 print(x_test.shape)
 
-# encoding_dim = 32 # 32 floats -> compression of factor 24.5, assuming input is 784 floats
-encoding_dim = int(3072/3) # 128 floats -> compression of factor 24.5, assuming input is 784 floats
+if use_cifar10: 
+    encoding_dim = int(3072*3) # N > M floats
+else: 
+    encoding_dim = 32 # 32 floats -> compression of factor 24.5, assuming input is 784 floats
 input_img = Input(shape=x_train.shape[1:])
 encoded = Dense(encoding_dim, activation='relu',
                 activity_regularizer=regularizers.l1(0.1))(input_img)
@@ -30,9 +36,9 @@ encoded_input = Input(shape=(encoding_dim,))
 decoder_layer = autoencoder.layers[-1]
 decoder = Model(encoded_input, decoder_layer(encoded_input))
 
-autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
+autoencoder.compile(optimizer='adam', loss='binary_crossentropy')
 
-autoencoder.fit(x_train, x_train, epochs=100, batch_size=256, shuffle=True, validation_data=(x_test,x_test))
+autoencoder.fit(x_train, x_train, epochs=50, batch_size=256, shuffle=True, validation_data=(x_test,x_test))
 
 encoded_imgs = encoder.predict(x_test)
 decoded_imgs = decoder.predict(encoded_imgs)
@@ -45,16 +51,18 @@ plt.figure(figsize=(20, 4))
 for i in range(n):
     # display original
     ax = plt.subplot(2, n, i + 1)
-    # plt.imshow(x_test[i].reshape(28, 28))
-    plt.imshow(x_test[i].reshape(32, 32, 3))
+    if use_cifar10: plt.imshow(x_test[i].reshape(32, 32, 3))
+    else: plt.imshow(x_test[i].reshape(28, 28))
+    
     plt.gray()
     ax.get_xaxis().set_visible(False)
     ax.get_yaxis().set_visible(False)
 
     # display reconstruction
     ax = plt.subplot(2, n, i + 1 + n)
-    # plt.imshow(decoded_imgs[i].reshape(28, 28))
-    plt.imshow(decoded_imgs[i].reshape(32, 32, 3))
+    
+    if use_cifar10: plt.imshow(decoded_imgs[i].reshape(32, 32, 3))
+    else: plt.imshow(decoded_imgs[i].reshape(28, 28))
     plt.gray()
     ax.get_xaxis().set_visible(False)
     ax.get_yaxis().set_visible(False)
