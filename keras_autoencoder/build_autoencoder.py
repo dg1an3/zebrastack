a11fed9478590f95cd570f1b3ff343f723aade1b
Derek Lane <dglane001@gmail.com>
2019-06-19 10:32:43 -0500

    adding explicit encode/decode pair use model summary (instead of explicit print statements) added plot_model

--- keras_autoencoder/build_autoencoder.py
+++ keras_autoencoder/build_autoencoder.py
@@ -1,58 +1,55 @@
 from keras.models import Model
 from keras.layers import Input, SpatialDropout2D, Conv2D, LocallyConnected2D, ZeroPadding2D, MaxPooling2D, UpSampling2D, ActivityRegularization
+from keras.utils import plot_model
 from keras import regularizers
 
 def build_autoencoder(sz, optimizer, loss):
     input_img = Input(shape=(sz,sz,1))
-    print(input_img.shape)
     x = Conv2D(32, (3,3), activation='relu', padding='same')(input_img)
-    print(x.shape)
     x = MaxPooling2D((2,2), padding='same')(x)
-    print(x.shape)
     x = SpatialDropout2D(0.1)(x)
-    print(x.shape)
     x = Conv2D(16, (3,3), activation='relu', padding='same')(x)
-    print(x.shape)
     x = MaxPooling2D((2,2), padding='same')(x)
-    print(x.shape)
     x = Conv2D(16, (3,3), activation='relu', padding='same')(x)
-    print(x.shape)
     x = MaxPooling2D((2,2), padding='same')(x)
-    print(x.shape)
     x = LocallyConnected2D(16, (3,3))(x)
-    print(x.shape)
     x = ZeroPadding2D(padding=(1,1))(x)
-    print(x.shape)
     x = MaxPooling2D((2,2), padding='same')(x)
-    print(x.shape)
-    encoded = ActivityRegularization(l1=5.0e-6,l2=1.0e-6)(x)
+    encoded = ActivityRegularization(l1=0.0e-4,l2=0.0e-4)(x)
 
     # TODO: add threshold layer for sparsity test
 
-    x = LocallyConnected2D(16, (3,3))(x)
-    print(x.shape)
+    x = LocallyConnected2D(16, (3,3))(encoded)
     x = ZeroPadding2D(padding=(1,1))(x)
-    print(x.shape)
     x = UpSampling2D((2,2))(x)
-    print(x.shape)
     x = Conv2D(16, (3,3), activation='relu', padding='same')(x)
-    print(x.shape)
     x = UpSampling2D((2,2))(x)
-    print(x.shape)
     x = Conv2D(16, (3,3), activation='relu', padding='same')(x)
-    print(x.shape)
     x = UpSampling2D((2,2))(x)
-    print(x.shape)
     x = Conv2D(32, (3,3), activation='relu', padding='same')(x)
-    print(x.shape)
     x = UpSampling2D((2,2))(x)
-    print(x.shape)
     decoded = Conv2D(1, (3,3), activation='sigmoid', padding='same')(x)
-    print(decoded.shape)
-
+    
     autoencoder = Model(input_img, decoded)
     autoencoder.compile(optimizer=optimizer, loss=loss)
+    autoencoder.summary()
+    plot_model(autoencoder, to_file='data\dicom_autoencoder.png', show_shapes=True)
 
     encode_only = Model(input_img, encoded)
-    return autoencoder, encode_only
+
+    encoded_input = Input(batch_shape=(None,encoded.shape[1].value,encoded.shape[2].value,encoded.shape[3].value))
+    x = autoencoder.layers[-10](encoded_input)
+    x = autoencoder.layers[-9](x)
+    x = autoencoder.layers[-8](x)
+    x = autoencoder.layers[-7](x)
+    x = autoencoder.layers[-6](x)
+    x = autoencoder.layers[-5](x)
+    x = autoencoder.layers[-4](x)
+    x = autoencoder.layers[-3](x)
+    x = autoencoder.layers[-2](x)
+    x = autoencoder.layers[-1](x)
+    decode_only = Model(encoded_input, x)
+
+    return autoencoder, encode_only, decode_only
+
 
