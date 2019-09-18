f85a7934f4b57188ce498fa605c73b4f2b7f4f38
Derek Lane <dglane001@gmail.com>
2019-09-18 14:16:45 -0500

    adding class to help with processing image

--- LittleCarVae/ProcessedImage.py
+++ LittleCarVae/ProcessedImage.py
@@ -1,28 +1,44 @@
+import numpy as np
 
 class ProcessedImage(object):
-	def __init__(self, fname, sz, color_depth):
+	def __init__(self, fname):
 		self.filename = fname
-		self.processed_size = sz
-		self.processed_depth = color_depth
-	
-	def process_image(self):
-		# load the image
+		self.cache = {}
+		self.original_array = None
 
-		# crop the image
+	def get_original(self):
+		# load the image, if not already
+		if not self.original_array:
+			self.original_array = np.array(10)
+		return self.original_array
 
-		# resize the image
+	def get_processed_image(self, sz, color_depth):
+		processed_array, _ = self.cache.get((sz,color_depth))
+		if not processed_array:
 
-		# whiten the image
-		self.prestats = 0.0, 0.0
+			original_array = self.get_original()
 
-		import numpy as np
-		self.processed_array = np.array(10)
+			# crop the image
+
+			# resize the image
+
+			# whiten the image
+			prestats = 0.0, 0.0
+			processed_array = np.array(10)
+
+			# add to the cache
+			self.cache[sz,color_depth] = processed_array, prestats
+
+		return processed_array
 
 	def reconstruct_from_predicted(self, predicted):
 		mean, std = self.prestats
-
-		import numpy as np
 		return np.array(4,4,2)
 
 	def render_image(self, ax, array=None):
 		pass
+
+	@static_method
+	def from_dir(dirname):
+		pass
+
