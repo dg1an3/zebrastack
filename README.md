d4885ec632551b39baf318c31867b48f36b3c0e6
Lane, Derek <Derek.Lane@elekta.com>
2020-08-22 17:04:48 -0500

    functional zebrastack_v0_covidnet, but starting to disassemble

--- README.md
+++ README.md
@@ -18,4 +18,12 @@ Other (non Cxr8) data
 - fashion_mnist.ipynb
 
 
-
+keras_autoencoder\build_autoencoder.py:from keras.models import Model      
+keras_autoencoder\keras_autoencoder.py:from keras.models import Model      
+anat0mixer\dynamic_routing_encoder.ipynb:    "from keras.models import Model\n",        
+anat0mixer\zebrastack_v0_covidnet.ipynb:    "from keras.models import Model\n",
+anat0mixer\zebrastack_v0_covidnet.ipynb:    "    encoder = keras.models.load_model('data\\zebrastack_v0_covidnet_encoder_model')\n",
+anat0mixer\zebrastack_v0_covidnet.ipynb:    "    decoder = keras.models.load_model('data\\zebrastack_v0_covidnet_decoder_model')"
+littlecarmash\model_vae_3stage.py:from tensorflow.keras.models import Model
+littlecarmash\LittleCarVae-cardb.ipynb:    "from keras.models import Model\n",
+littlecarmash\LittleCarVae.ipynb:    "from keras.models import Model\n",     
--- anat0mixer/show_original_decoded.py
+++ anat0mixer/show_original_decoded.py
@@ -1,4 +1,18 @@
 import matplotlib.pyplot as plt
+
+def decode_latent(decoder, latent_vector:tuple):
+    """
+    helper to decode latent, with caching to speed up
+    # Arguments
+        decode_only: model for decoding
+        latent_vector: _tuple_ representing the vector to be decoded
+    # Returns
+        decoded image
+    """
+    import numpy as np
+    latent_vector_arr = np.array([latent_vector])
+    return decoder.predict(latent_vector_arr)
+
 def show_grayscale(rows, columns, at, pixel_array, sz):
     import scipy
     ax = plt.subplot(rows, columns, at)
@@ -7,8 +21,7 @@ def show_grayscale(rows, columns, at, pixel_array, sz):
     ax.get_xaxis().set_visible(False)
     ax.get_yaxis().set_visible(False)
 
-def show_original_decoded(original, decoded, sz):
-    n = 10  # how many figures we will display
+def show_original_decoded(original, decoded, sz, n=10):
     plt.figure(figsize=(n*2, 4))
     for i in range(n):
         show_grayscale(2, n, i+1, original[i], sz)
