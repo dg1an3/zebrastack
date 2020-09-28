2f04391c792d38d39726eab481d407e3e1f60e59
Lane, Derek <Derek.Lane@elekta.com>
2020-09-28 15:37:46 -0500

    reconstructing from gabor filter bank

--- zebrastack/common/filter_banks.py
+++ zebrastack/common/filter_banks.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
 import reprlib
+import logging
 
 def grid_for_sz(sz=9):
     """ """
@@ -41,29 +42,62 @@ def wave_numbers(count):
 def make_sine_kernels(sz=9, ks=[(1.0,0.0)], freqs=[1.0]):
     """ """    
     xs, ys = grid_for_sz(sz)
-    sine_kernels = []
-    for phase in [xs*k[0] + ys*k[1] for k in ks]:
-        sine_kernels += [np.sin(freq * phase) for freq in freqs]
-    return kernel_list_to_tf(sine_kernels)
+    sine_kernels_phi_0 = []
+    sine_kernels_phi_hpi = []
+    for freq in freqs:
+        for k in ks:        
+            print(f"k={k} freq={freq} phase={0.0}")
+            sine_kernels_phi_0 \
+                .append(np.sin(freq * (xs*k[0] + ys*k[1]) + 0.0))
+            print(f"k={k} freq={freq} phase={np.pi/2.}")
+            sine_kernels_phi_hpi \
+                .append(np.sin(freq * (xs*k[0] + ys*k[1]) + (np.pi/2.)))
+    return (kernel_list_to_tf(sine_kernels_phi_0), \
+                kernel_list_to_tf(sine_kernels_phi_hpi))
 
 def make_gabor_kernels(sz=9, ks=[(1.0,0.0)], freqs=[1.0]):
-    sine_kernels = make_sine_kernels(sz=sz, ks=ks, freqs=freqs)
-    gauss_kernels = make_gauss_kernels(sz=sz, sigmas=freqs)
-    gauss_kernels = np.repeat(gauss_kernels, 
-                              len(sine_kernels) // len(gauss_kernels), axis=0)
-    logging.info(f"Gaussien kernel shape = {gauss_kernels.shape}, "
-                     + f"Sine kernel shape = {sine_kernels.shape}")
-    return gauss_kernels * sine_kernels
+    sine_kernels_phi_0, sine_kernels_phi_hpi = \
+        make_sine_kernels(sz=sz, ks=ks, freqs=freqs)
+    logging.info(f"Sine kernels shape = {sine_kernels_phi_0.shape}"
+                 + f"{sine_kernels_phi_hpi.shape}")
+    
+    gauss_kernels = make_gauss_kernels(sz=sz, sigmas=[(2./f) for f in freqs])
+    gauss_kernels = \
+        np.repeat(gauss_kernels, 
+                  sine_kernels_phi_0.shape[-1] // gauss_kernels.shape[-1], axis=-1)
+    logging.info(f"Gaussian kernel shape = {gauss_kernels.shape}, "
+                     + f"Sine kernel shape = {sine_kernels_phi_0.shape}")
+    return (gauss_kernels * sine_kernels_phi_0, gauss_kernels * sine_kernels_phi_hpi)
 
 def show_filter_bank(filter_bank:tf.Tensor, rows=1):
     filter_count = filter_bank.shape[-1]
     per_row = filter_count // rows
-    print(filter_count, per_row)
+    assert rows * per_row == filter_count
+    
+    logging.info(f"Showing {filter_count} filters in {per_row} rows")
     fig, axs = plt.subplots(rows,per_row,figsize=(5,2))
     for n in range(filter_count):
         filter = tf.reshape(filter_bank[...,0,n], filter_bank.shape[:2])
         if rows > 1:
-            axs[n // rows][n % per_row].imshow(filter, cmap='plasma')
+            logging.info(f"{n}:{n // per_row} {n % per_row}")
+            axs[n // per_row][n % per_row].imshow(filter, cmap='plasma')
         else:
             axs[n].imshow(filter, cmap='plasma')
+
+def show_filter_response(filter_response:tf.Tensor, rows=1):
+    filter_count = filter_response.shape[-1]
+    per_row = filter_count // rows
+    assert rows * per_row == filter_count
+    
+    logging.info(f"Showing {filter_count} filters in {per_row} rows")
+    fig, axs = plt.subplots(rows,per_row,figsize=(5,2))
+    for n in range(filter_count):
+        logging.info(f"filter_response.shape = {filter_response.shape})")
+        filter = tf.reshape(filter_response[...,n], 
+                            (filter_response.shape[1], filter_response.shape[2]))
+        if rows > 1:
+            logging.info(f"{n}:{n // per_row} {n % per_row}")
+            axs[n // per_row][n % per_row].imshow(filter, cmap='plasma')
+        else:
+            axs[n].imshow(filter, cmap='plasma')            
         
